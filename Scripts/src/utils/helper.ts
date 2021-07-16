86c47e569c6075b2ebd380806aa6b256a7e88736
Marcus Lee <marcustutorial@hotmail.com>
2021-07-16 17:22:57 +0800

    Added helper.ts

---
+++ Scripts/src/utils/helper.ts
@@ -0,0 +1,30 @@
+﻿/**
+ * Slice a 1-dimensional array into chunks of arrays in a 2-dimensional array
+ * @param {Array<T>} arr - the input array
+ * @param {number} chunkSize - desired chunk size
+ * @returns {Array<Array<T>>} 2D array consisting chunks of the original array
+ */
+export const sliceIntoChunks = <T>(arr: T[], chunkSize: number): T[][] => {
+  const res: T[][] = [];
+  for (let i = 0; i < arr.length; i += chunkSize) {
+    const chunk = arr.slice(i, i + chunkSize);
+    res.push(chunk);
+  }
+  return res;
+};
+
+/**
+ * Pause execution for specified duration
+ * @param {number} ms - sleep duration in milliseconds
+ * @returns {Prmoise<unknown>} a Promise that resolves in the given duration
+ */
+export const sleep = (ms: number) =>
+  new Promise((resolve) => setTimeout(resolve, ms));
+
+/**
+ * Retrieve the state injected by .NET
+ * @param {string} id - the hidden field's id
+ * @returns {T} a typed state object
+ */
+export const getStateFromBackend = <T>(id: string) =>
+  JSON.parse((<HTMLInputElement>document.getElementById(id)).value) as T;
